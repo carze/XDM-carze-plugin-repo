4e7d0c671b50ad033260794e5805ab30c93cb94b
Cesar <cesar.arze@gmail.com>
2014-03-19 20:32:13 -0400

    Fixing some issues with identifiers breaking ShowRenamer plugin

--- ShowRenamer/showrenamer.py
+++ ShowRenamer/showrenamer.py
@@ -31,7 +31,7 @@ class ModifyName(SearchTermFilter):
     but allow replacing of the <TITLE> element.
     """
     version = "0.1"
-    identifier = 'de.carze.element.name'
+    identifier = 'de.carze.element.showrenamer'
     screenName = 'Replace Name To Look For'
     addMediaTypeOptions = "runFor"
     single = True
